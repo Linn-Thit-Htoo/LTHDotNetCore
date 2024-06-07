aa48669e0924869d720d381f68ce98a6b5ab95d9
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-07 21:23:15 +0630

    -

--- LTHDotNetCore.BlazarWasm/Pages/BlogPage/BlogCreate.razor.cs
+++ LTHDotNetCore.BlazarWasm/Pages/BlogPage/BlogCreate.razor.cs
@@ -4,23 +4,22 @@ using Newtonsoft.Json;
 using System.Net.Mime;
 using System.Text;
 
-namespace LTHDotNetCore.BlazarWasm.Pages.BlogPage
+namespace LTHDotNetCore.BlazarWasm.Pages.BlogPage;
+
+public partial class BlogCreate
 {
-    public partial class BlogCreate
+    private BlogDataModel requestModel = new();
+    private async Task Save()
     {
-        private BlogDataModel requestModel = new();
-        private async Task Save()
+        var blogJson = JsonConvert.SerializeObject(requestModel);
+        HttpContent content = new StringContent(blogJson, Encoding.UTF8, MediaTypeNames.Application.Json);
+        var response = await HttpClient.PostAsync("api/BlogEFCore", content);
+        if (response.IsSuccessStatusCode)
         {
-            var blogJson = JsonConvert.SerializeObject(requestModel);
-            HttpContent content = new StringContent(blogJson, Encoding.UTF8, MediaTypeNames.Application.Json);
-            var response = await HttpClient.PostAsync("api/BlogEFCore", content);
-            if (response.IsSuccessStatusCode)
-            {
-                var message = await response.Content.ReadAsStringAsync();
-                //await JsRuntime.InvokeVoidAsync("alert", message);
-                Snackbar.Add(message, Severity.Success);
-                Nav.NavigateTo("/setup/blog");
-            }
+            var message = await response.Content.ReadAsStringAsync();
+            //await JsRuntime.InvokeVoidAsync("alert", message);
+            Snackbar.Add(message, Severity.Success);
+            Nav.NavigateTo("/setup/blog");
         }
     }
 }
