442c6889826a54a27e86d11f8e288433503ec62d
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-07 21:37:53 +0630

    -

--- LTHDotNetCore.ConsoleApp/HttpClientExamples/HttpClientExample.cs
+++ LTHDotNetCore.ConsoleApp/HttpClientExamples/HttpClientExample.cs
@@ -3,167 +3,166 @@ using Newtonsoft.Json;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace LTHDotNetCore.ConsoleApp.HttpClientExamples
+namespace LTHDotNetCore.ConsoleApp.HttpClientExamples;
+
+public class HttpClientExample
 {
-    public class HttpClientExample
+    private string _blogEndpoint = "https://localhost:7267/api/BlogDapper";
+    public async Task Run()
     {
-        private string _blogEndpoint = "https://localhost:7267/api/BlogDapper";
-        public async Task Run()
-        {
-            //await Read();
-            await Edit(6);
-            //await Create("Http client", "hHttp author", "Http Content");
-            //await Update(10, "Http title edited", "http author edited", "http content edited");
-            //await Patch(10, "Http title patched", "http author patched");
-            //await Delete(1);
-        }
+        //await Read();
+        await Edit(6);
+        //await Create("Http client", "hHttp author", "Http Content");
+        //await Update(10, "Http title edited", "http author edited", "http content edited");
+        //await Patch(10, "Http title patched", "http author patched");
+        //await Delete(1);
+    }
 
-        #region Read
-        public async Task Read()
+    #region Read
+    public async Task Read()
+    {
+        try
         {
-            try
+            HttpClient client = new();
+            HttpResponseMessage response = await client.GetAsync(_blogEndpoint);
+            if (response.IsSuccessStatusCode)
             {
-                HttpClient client = new();
-                HttpResponseMessage response = await client.GetAsync(_blogEndpoint);
-                if (response.IsSuccessStatusCode)
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                List<BlogDataModel> lst = JsonConvert.DeserializeObject<List<BlogDataModel>>(jsonStr)!;
+                foreach (BlogDataModel item in lst)
                 {
-                    string jsonStr = await response.Content.ReadAsStringAsync();
-                    List<BlogDataModel> lst = JsonConvert.DeserializeObject<List<BlogDataModel>>(jsonStr)!;
-                    foreach (BlogDataModel item in lst)
-                    {
-                        Console.WriteLine(item.Blog_Id);
-                        Console.WriteLine(item.Blog_Title);
-                        Console.WriteLine(item.Blog_Author);
-                        Console.WriteLine(item.Blog_Content);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        #endregion
-
-        #region Edit
-        public async Task Edit(int id)
-        {
-            try
-            {
-                HttpClient client = new();
-                HttpResponseMessage response = await client.GetAsync($"{_blogEndpoint}/{id}");
-                if (response.IsSuccessStatusCode)
-                {
-                    string jsonStr = await response.Content.ReadAsStringAsync();
-                    BlogDataModel item = JsonConvert.DeserializeObject<BlogDataModel>(jsonStr)!;
-
                     Console.WriteLine(item.Blog_Id);
                     Console.WriteLine(item.Blog_Title);
                     Console.WriteLine(item.Blog_Author);
                     Console.WriteLine(item.Blog_Content);
                 }
             }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
         }
-        #endregion
-
-        #region Create
-        public async Task Create(string title, string author, string content)
+        catch (Exception ex)
         {
-            try
-            {
-                var blog = new BlogDataModel()
-                {
-                    Blog_Title = title,
-                    Blog_Author = author,
-                    Blog_Content = content
-                };
-                string jsonBlog = JsonConvert.SerializeObject(blog);
-                HttpContent httpContent = new StringContent(jsonBlog, Encoding.UTF8, Application.Json);
-
-                HttpClient client = new();
-                HttpResponseMessage response = await client.PostAsync(_blogEndpoint, httpContent);
-
-                await Console.Out.WriteLineAsync(await response.Content.ReadAsStringAsync());
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
         }
-        #endregion
-
-        #region Update (put)
-        public async Task Update(int id, string title, string author, string content)
-        {
-            try
-            {
-                var blog = new BlogDataModel()
-                {
-                    Blog_Id = id,
-                    Blog_Title = title,
-                    Blog_Author = author,
-                    Blog_Content = content
-                };
-                string jsonBlog = JsonConvert.SerializeObject(blog);
-                HttpContent httpContent = new StringContent(jsonBlog, Encoding.UTF8, Application.Json);
-
-                HttpClient client = new HttpClient();
-                HttpResponseMessage response = await client.PutAsync($"{_blogEndpoint}/{id}", httpContent);
-
-                Console.WriteLine(await response.Content.ReadAsStringAsync());
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        #endregion
-
-        #region Patch
-        public async Task Patch(int id, string title, string author)
-        {
-            try
-            {
-                var blog = new BlogDataModel()
-                {
-                    Blog_Id = id,
-                    Blog_Title = title,
-                    Blog_Author = author
-                };
-                string jsonBlog = JsonConvert.SerializeObject(blog);
-                HttpContent httpContent = new StringContent(jsonBlog, Encoding.UTF8, Application.Json);
-
-                HttpClient client = new();
-                HttpResponseMessage response = await client.PatchAsync($"{_blogEndpoint}/{id}", httpContent);
-
-                Console.WriteLine(await response.Content.ReadAsStringAsync());
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        #endregion
-
-        #region Delete
-        public async Task Delete(int id)
-        {
-            try
-            {
-                HttpClient client = new();
-                HttpResponseMessage response = await client.DeleteAsync($"{_blogEndpoint}/{id}");
-
-                Console.WriteLine(await response.Content.ReadAsStringAsync());
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        #endregion
     }
+    #endregion
+
+    #region Edit
+    public async Task Edit(int id)
+    {
+        try
+        {
+            HttpClient client = new();
+            HttpResponseMessage response = await client.GetAsync($"{_blogEndpoint}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                BlogDataModel item = JsonConvert.DeserializeObject<BlogDataModel>(jsonStr)!;
+
+                Console.WriteLine(item.Blog_Id);
+                Console.WriteLine(item.Blog_Title);
+                Console.WriteLine(item.Blog_Author);
+                Console.WriteLine(item.Blog_Content);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    #endregion
+
+    #region Create
+    public async Task Create(string title, string author, string content)
+    {
+        try
+        {
+            var blog = new BlogDataModel()
+            {
+                Blog_Title = title,
+                Blog_Author = author,
+                Blog_Content = content
+            };
+            string jsonBlog = JsonConvert.SerializeObject(blog);
+            HttpContent httpContent = new StringContent(jsonBlog, Encoding.UTF8, Application.Json);
+
+            HttpClient client = new();
+            HttpResponseMessage response = await client.PostAsync(_blogEndpoint, httpContent);
+
+            await Console.Out.WriteLineAsync(await response.Content.ReadAsStringAsync());
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    #endregion
+
+    #region Update (put)
+    public async Task Update(int id, string title, string author, string content)
+    {
+        try
+        {
+            var blog = new BlogDataModel()
+            {
+                Blog_Id = id,
+                Blog_Title = title,
+                Blog_Author = author,
+                Blog_Content = content
+            };
+            string jsonBlog = JsonConvert.SerializeObject(blog);
+            HttpContent httpContent = new StringContent(jsonBlog, Encoding.UTF8, Application.Json);
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.PutAsync($"{_blogEndpoint}/{id}", httpContent);
+
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    #endregion
+
+    #region Patch
+    public async Task Patch(int id, string title, string author)
+    {
+        try
+        {
+            var blog = new BlogDataModel()
+            {
+                Blog_Id = id,
+                Blog_Title = title,
+                Blog_Author = author
+            };
+            string jsonBlog = JsonConvert.SerializeObject(blog);
+            HttpContent httpContent = new StringContent(jsonBlog, Encoding.UTF8, Application.Json);
+
+            HttpClient client = new();
+            HttpResponseMessage response = await client.PatchAsync($"{_blogEndpoint}/{id}", httpContent);
+
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    #endregion
+
+    #region Delete
+    public async Task Delete(int id)
+    {
+        try
+        {
+            HttpClient client = new();
+            HttpResponseMessage response = await client.DeleteAsync($"{_blogEndpoint}/{id}");
+
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    #endregion
 }
