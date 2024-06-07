e2d499a31f5136cd2e94cee21c12b5c0b3cb0501
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-07 21:23:37 +0630

    -

--- LTHDotNetCore.BlazarWasm/Pages/BlogPage/BlogEdit.razor.cs
+++ LTHDotNetCore.BlazarWasm/Pages/BlogPage/BlogEdit.razor.cs
@@ -5,47 +5,46 @@ using Newtonsoft.Json;
 using System.Net.Mime;
 using System.Text;
 
-namespace LTHDotNetCore.BlazarWasm.Pages.BlogPage
-{
-    public partial class BlogEdit
-    {
-        [Parameter]
-        public int id { get; set; }
+namespace LTHDotNetCore.BlazarWasm.Pages.BlogPage;
 
-        private BlogDataModel item;
-        protected override async Task OnInitializedAsync()
+public partial class BlogEdit
+{
+    [Parameter]
+    public int id { get; set; }
+
+    private BlogDataModel item;
+    protected override async Task OnInitializedAsync()
+    {
+        var result = await HttpClient.GetAsync("api/BlogEFCore/" + id);
+        if (result.IsSuccessStatusCode)
         {
-            var result = await HttpClient.GetAsync("api/BlogEFCore/" + id);
-            if (result.IsSuccessStatusCode)
+            string jsonStr = await result.Content.ReadAsStringAsync();
+            item = JsonConvert.DeserializeObject<BlogDataModel>(jsonStr)!;
+        }
+    }
+    private async Task Update()
+    {
+        try
+        {
+            string jsonStr = JsonConvert.SerializeObject(item);
+            HttpContent content = new StringContent(jsonStr, Encoding.UTF8, MediaTypeNames.Application.Json);
+            var response = await HttpClient.PutAsync($"api/BlogEFCore/{id}", content);
+            if (response.IsSuccessStatusCode)
             {
-                string jsonStr = await result.Content.ReadAsStringAsync();
-                item = JsonConvert.DeserializeObject<BlogDataModel>(jsonStr)!;
+                var message = await response.Content.ReadAsStringAsync();
+                Snackbar.Add(message, Severity.Success);
+                Nav.NavigateTo("/setup/blog");
+            }
+            else
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                Snackbar.Add(message, Severity.Error);
+                Nav.NavigateTo("/setup/blog");
             }
         }
-        private async Task Update()
+        catch
         {
-            try
-            {
-                string jsonStr = JsonConvert.SerializeObject(item);
-                HttpContent content = new StringContent(jsonStr, Encoding.UTF8, MediaTypeNames.Application.Json);
-                var response = await HttpClient.PutAsync($"api/BlogEFCore/{id}", content);
-                if (response.IsSuccessStatusCode)
-                {
-                    var message = await response.Content.ReadAsStringAsync();
-                    Snackbar.Add(message, Severity.Success);
-                    Nav.NavigateTo("/setup/blog");
-                }
-                else
-                {
-                    var message = await response.Content.ReadAsStringAsync();
-                    Snackbar.Add(message, Severity.Error);
-                    Nav.NavigateTo("/setup/blog");
-                }
-            }
-            catch
-            {
-                throw;
-            }
+            throw;
         }
     }
 }
