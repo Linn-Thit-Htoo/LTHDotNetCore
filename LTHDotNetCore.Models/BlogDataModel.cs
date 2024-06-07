28ac68ba533a705fa6f6e8389f854ad649288647
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-07 21:22:14 +0630

    -

--- LTHDotNetCore.Models/BlogDataModel.cs
+++ LTHDotNetCore.Models/BlogDataModel.cs
@@ -1,15 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace LTHDotNetCore.Models
+namespace LTHDotNetCore.Models;
+
+[Table("Tbl_Blog")]
+public class BlogDataModel
 {
-    [Table("Tbl_Blog")]
-    public class BlogDataModel
-    {
-        [Key]
-        public int Blog_Id { get; set; }
-        public string? Blog_Title { get; set; }
-        public string? Blog_Author { get; set; }
-        public string? Blog_Content { get; set; }
-    }
+    [Key]
+    public int Blog_Id { get; set; }
+    public string? Blog_Title { get; set; }
+    public string? Blog_Author { get; set; }
+    public string? Blog_Content { get; set; }
 }
