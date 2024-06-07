3a2e56c137fe34bf752339528dbd1b9f90c5afaf
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-07 21:15:44 +0630

    -

--- LTHDotNetCore.RestApi/Middlewares/LogHeadersMiddleware.cs
+++ LTHDotNetCore.RestApi/Middlewares/LogHeadersMiddleware.cs
@@ -1,32 +1,31 @@
-﻿namespace LTHDotNetCore.RestApi.Middlewares
+﻿namespace LTHDotNetCore.RestApi.Middlewares;
+
+public class LogHeadersMiddleware
 {
-    public class LogHeadersMiddleware
+    private readonly RequestDelegate _next;
+    private readonly ILogger<LogHeadersMiddleware> _logger;
+
+    public LogHeadersMiddleware(RequestDelegate next, ILogger<LogHeadersMiddleware> logger)
     {
-        private readonly RequestDelegate _next;
-        private readonly ILogger<LogHeadersMiddleware> _logger;
-
-        public LogHeadersMiddleware(RequestDelegate next, ILogger<LogHeadersMiddleware> logger)
-        {
-            _next = next;
-            _logger = logger;
-        }
-
-        public async Task InvokeAsync(HttpContext context)
-        {
-            foreach (var header in context.Request.Headers)
-            {
-                _logger.LogInformation("Header: {Key}: {Value}", header.Key, header.Value);
-            }
-
-            await _next(context);
-        }
+        _next = next;
+        _logger = logger;
     }
 
-    public static class LogHeadersMiddlewareExtensions
+    public async Task InvokeAsync(HttpContext context)
     {
-        public static IApplicationBuilder UseLogHeaders(this IApplicationBuilder builder)
+        foreach (var header in context.Request.Headers)
         {
-            return builder.UseMiddleware<LogHeadersMiddleware>();
+            _logger.LogInformation("Header: {Key}: {Value}", header.Key, header.Value);
         }
+
+        await _next(context);
+    }
+}
+
+public static class LogHeadersMiddlewareExtensions
+{
+    public static IApplicationBuilder UseLogHeaders(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<LogHeadersMiddleware>();
     }
 }
