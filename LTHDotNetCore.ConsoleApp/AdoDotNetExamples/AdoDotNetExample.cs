9f662ad8c8cebd1baadab76dca4cfc5dc6853d9a
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-07 21:26:23 +0630

    -

--- LTHDotNetCore.ConsoleApp/AdoDotNetExamples/AdoDotNetExample.cs
+++ LTHDotNetCore.ConsoleApp/AdoDotNetExamples/AdoDotNetExample.cs
@@ -1,86 +1,119 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 
-namespace LTHDotNetCore.ConsoleApp.AdoDotNetExamples
-{
-    public class AdoDotNetExample
-    {
-        public void Run()
-        {
-            //Read();
-            //Edit(1);
-            //Create("Title","Author","Content");
-            //Update(1);
-            Delete(2);
-        }
+namespace LTHDotNetCore.ConsoleApp.AdoDotNetExamples;
 
-        #region Read
-        private void Read()
+public class AdoDotNetExample
+{
+    public void Run()
+    {
+        //Read();
+        //Edit(1);
+        //Create("Title","Author","Content");
+        //Update(1);
+        Delete(2);
+    }
+
+    #region Read
+    private void Read()
+    {
+        SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
         {
-            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-            {
-                DataSource = "LAPTOP-DR9SFJ1C",
-                InitialCatalog = "DotNetClass",
-                UserID = "sa",
-                Password = "sa@123",
-            };
-            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-            connection.Open();
-            string query = @"SELECT [Blog_Id]
+            DataSource = "LAPTOP-DR9SFJ1C",
+            InitialCatalog = "DotNetClass",
+            UserID = "sa",
+            Password = "sa@123",
+        };
+        SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        string query = @"SELECT [Blog_Id]
       ,[Blog_Title]
       ,[Blog_Author]
       ,[Blog_Content]
   FROM [dbo].[Tbl_blog]";
-            SqlCommand command = new SqlCommand(query, connection);
-            DataTable dt = new DataTable();
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
-            sqlDataAdapter.Fill(dt);
-            connection.Close();
-            foreach (DataRow dr in dt.Rows)
-            {
-                Console.WriteLine($"Id => {dr["Blog_Id"]}");
-                Console.WriteLine($"Blog Title {dr["Blog_Title"]}");
-                Console.WriteLine($"Blog Author {dr["Blog_Author"]}");
-                Console.WriteLine($"Blog Content {dr["Blog_Content"]}");
-                Console.WriteLine("\n");
-            }
-        }
-        #endregion
-
-        #region Edit
-        private void Edit(int id)
+        SqlCommand command = new SqlCommand(query, connection);
+        DataTable dt = new DataTable();
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+        sqlDataAdapter.Fill(dt);
+        connection.Close();
+        foreach (DataRow dr in dt.Rows)
         {
-            SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
-            {
-                DataSource = "LAPTOP-DR9SFJ1C",
-                InitialCatalog = "DotNetClass",
-                UserID = "sa",
-                Password = "sa@123"
-            };
-            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-            connection.Open();
-            string query = @"SELECT [Blog_Id]
+            Console.WriteLine($"Id => {dr["Blog_Id"]}");
+            Console.WriteLine($"Blog Title {dr["Blog_Title"]}");
+            Console.WriteLine($"Blog Author {dr["Blog_Author"]}");
+            Console.WriteLine($"Blog Content {dr["Blog_Content"]}");
+            Console.WriteLine("\n");
+        }
+    }
+    #endregion
+
+    #region Edit
+    private void Edit(int id)
+    {
+        SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
+        {
+            DataSource = "LAPTOP-DR9SFJ1C",
+            InitialCatalog = "DotNetClass",
+            UserID = "sa",
+            Password = "sa@123"
+        };
+        SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        string query = @"SELECT [Blog_Id]
       ,[Blog_Title]
       ,[Blog_Author]
       ,[Blog_Content]
   FROM [dbo].[Tbl_blog]
   WHERE Blog_Id = @Blog_Id;";
-            SqlCommand cmd = new(query, connection);
-            cmd.Parameters.AddWithValue("@Blog_Id", id);
-            DataTable dt = new();
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-            sqlDataAdapter.Fill(dt);
-            connection.Close();
-            DataRow row = dt.Rows[0];
-            Console.WriteLine($"Id => {row["Blog_Id"]}");
-            Console.WriteLine($"Blog Title => {row["Blog_Title"]}");
-            Console.WriteLine($"Blog Author => {row["Blog_Author"]}");
-            Console.WriteLine($"Blog Content => {row["Blog_Content"]}");
-        }
-        #endregion
+        SqlCommand cmd = new(query, connection);
+        cmd.Parameters.AddWithValue("@Blog_Id", id);
+        DataTable dt = new();
+        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+        sqlDataAdapter.Fill(dt);
+        connection.Close();
+        DataRow row = dt.Rows[0];
+        Console.WriteLine($"Id => {row["Blog_Id"]}");
+        Console.WriteLine($"Blog Title => {row["Blog_Title"]}");
+        Console.WriteLine($"Blog Author => {row["Blog_Author"]}");
+        Console.WriteLine($"Blog Content => {row["Blog_Content"]}");
+    }
+    #endregion
 
-        #region Create
-        private void Create(string title, string author, string content)
+    #region Create
+    private void Create(string title, string author, string content)
+    {
+        SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
+        {
+            DataSource = "LAPTOP-DR9SFJ1C",
+            InitialCatalog = "DotNetClass",
+            UserID = "sa",
+            Password = "sa@123"
+        };
+        SqlConnection connection = new(sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        string query = @"INSERT INTO [dbo].[Tbl_blog]
+    ([Blog_Title]
+    ,[Blog_Author]
+    ,[Blog_Content])
+VALUES (@Blog_Title
+           ,@Blog_Author
+           ,@Blog_Content);
+";
+        SqlCommand command = new(query, connection);
+        command.Parameters.AddWithValue("@Blog_Title", title);
+        command.Parameters.AddWithValue("@Blog_Author", author);
+        command.Parameters.AddWithValue("@Blog_Content", content);
+        int result = command.ExecuteNonQuery();
+        connection.Close();
+        string message = result > 0 ? "Inserted Successfully!" : "Insert data fail!";
+        Console.WriteLine(message);
+    }
+    #endregion
+
+    #region Update
+    private void Update(int id, string title, string author, string content)
+    {
+        try
         {
             SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
             {
@@ -91,89 +124,55 @@ namespace LTHDotNetCore.ConsoleApp.AdoDotNetExamples
             };
             SqlConnection connection = new(sqlConnectionStringBuilder.ConnectionString);
             connection.Open();
-            string query = @"INSERT INTO [dbo].[Tbl_blog]
-    ([Blog_Title]
-    ,[Blog_Author]
-    ,[Blog_Content])
-VALUES (@Blog_Title
-           ,@Blog_Author
-           ,@Blog_Content);
-";
-            SqlCommand command = new(query, connection);
-            command.Parameters.AddWithValue("@Blog_Title", title);
-            command.Parameters.AddWithValue("@Blog_Author", author);
-            command.Parameters.AddWithValue("@Blog_Content", content);
-            int result = command.ExecuteNonQuery();
-            connection.Close();
-            string message = result > 0 ? "Inserted Successfully!" : "Insert data fail!";
-            Console.WriteLine(message);
-        }
-        #endregion
-
-        #region Update
-        private void Update(int id, string title, string author, string content)
-        {
-            try
-            {
-                SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
-                {
-                    DataSource = "LAPTOP-DR9SFJ1C",
-                    InitialCatalog = "DotNetClass",
-                    UserID = "sa",
-                    Password = "sa@123"
-                };
-                SqlConnection connection = new(sqlConnectionStringBuilder.ConnectionString);
-                connection.Open();
-                string query = @"UPDATE [dbo].[Tbl_blog]
+            string query = @"UPDATE [dbo].[Tbl_blog]
    SET [Blog_Title] = @Blog_Title
       ,[Blog_Author] = @Blog_Author
       ,[Blog_Content] = @Blog_Content
  WHERE Blog_Id = @Blog_Id;";
-                SqlCommand cmd = new(query, connection);
-                cmd.Parameters.AddWithValue("@BLog_Title", title);
-                cmd.Parameters.AddWithValue("@Blog_Author", author);
-                cmd.Parameters.AddWithValue("@Blog-Content", content);
-                cmd.Parameters.AddWithValue("@Blog_Id", id);
-                int result = cmd.ExecuteNonQuery();
-                connection.Close();
-                string message = result > 0 ? "Updated Successfully!" : "Update Fail!";
-                Console.WriteLine(message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            SqlCommand cmd = new(query, connection);
+            cmd.Parameters.AddWithValue("@BLog_Title", title);
+            cmd.Parameters.AddWithValue("@Blog_Author", author);
+            cmd.Parameters.AddWithValue("@Blog-Content", content);
+            cmd.Parameters.AddWithValue("@Blog_Id", id);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+            string message = result > 0 ? "Updated Successfully!" : "Update Fail!";
+            Console.WriteLine(message);
         }
-        #endregion
-
-        #region Delete
-        private void Delete(int id)
+        catch (Exception ex)
         {
-            try
-            {
-                SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
-                {
-                    DataSource = "LAPTOP-DR9SFJ1C",
-                    InitialCatalog = "DotNetClass",
-                    UserID = "sa",
-                    Password = "sa@123"
-                };
-                SqlConnection connection = new(sqlConnectionStringBuilder.ConnectionString);
-                connection.Open();
-                string query = @"DELETE FROM [dbo].[Tbl_blog]
-      WHERE Blog_Id = @Blog_Id;";
-                SqlCommand command = new(query, connection);
-                command.Parameters.AddWithValue("@Blog_Id", id);
-                int result = command.ExecuteNonQuery();
-                connection.Close();
-                string message = result > 0 ? "Deleted Successfully!" : "Delete data fail!";
-                Console.WriteLine(message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Console.WriteLine(ex.Message);
         }
-        #endregion
     }
+    #endregion
+
+    #region Delete
+    private void Delete(int id)
+    {
+        try
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new()
+            {
+                DataSource = "LAPTOP-DR9SFJ1C",
+                InitialCatalog = "DotNetClass",
+                UserID = "sa",
+                Password = "sa@123"
+            };
+            SqlConnection connection = new(sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"DELETE FROM [dbo].[Tbl_blog]
+      WHERE Blog_Id = @Blog_Id;";
+            SqlCommand command = new(query, connection);
+            command.Parameters.AddWithValue("@Blog_Id", id);
+            int result = command.ExecuteNonQuery();
+            connection.Close();
+            string message = result > 0 ? "Deleted Successfully!" : "Delete data fail!";
+            Console.WriteLine(message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    #endregion
 }
